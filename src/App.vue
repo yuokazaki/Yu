98bb2225267c70c19114db4538d78c5899db6ff0
岡崎悠 <okazakiyu@okazakiyuunoMacBook-Air.local>
2020-04-08 18:07:00 +0900

    PR20200408

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,39 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header />
+    <Main />
+    <About />
+    <Skill />
+    <Vision />
+    <Footer />
+    <Menu />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue'
+import Main from './components/Main.vue'
+import About from './components/About.vue'
+import Skill from './components/Skill.vue'
+import Vision from './components/Vision.vue'
+import Footer from './components/Footer.vue'
+import Menu from './components/Footer.vue'
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Header,
+    Main,
+    About,
+    Skill,
+    Vision,
+    Footer,
+    Menu
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/Menu.vue
@@ -0,0 +1,18 @@
+<template>
+<div id="nav-content">
+  <ul>
+    <li>Home</li>
+    <li>About Me</li>
+    <li>Skill Sets</li>
+    <li>Vision</li>
+    <li>Customize Item1</li>
+    <li>Customize Item2</li>
+  </ul>
+</div>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
