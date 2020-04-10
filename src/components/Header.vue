4647a3561f628761e7011fa22c68f45ed887e2b7
岡崎悠 <okazakiyu@okazakiyuunoMacBook-Air.local>
2020-04-10 14:11:36 +0900

    Lint自動設定の追加

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,12 +1,23 @@
 <template>
   <div id="headerSection">
     <div id="nav-drawer">
-      <input id="nav-input" type="checkbox" class="nav-unshown">
-      <label id="nav-open" for="nav-input"><span></span></label>
-      <label class="nav-unshown" id="nav-close" for="nav-input"></label>
-        <div id="nav-content">
-          <Menu />
-        </div>
+      <input
+        id="nav-input"
+        type="checkbox"
+        class="nav-unshown"
+      >
+      <label
+        id="nav-open"
+        for="nav-input"
+      ><span /></label>
+      <label
+        id="nav-close"
+        class="nav-unshown"
+        for="nav-input"
+      />
+      <div id="nav-content">
+        <Menu />
+      </div>
     </div>
   </div>
 </template>
@@ -25,10 +36,10 @@ export default {
 
 <style scoped>
 #headerSection {
-  background-color: #F3F3F3;
+  background-color: #f3f3f3;
   width: 100%;
   height: auto;
-  padding:10px;
+  padding: 10px;
 }
 
 #headerButton {
@@ -43,12 +54,12 @@ export default {
   position: relative;
 }
 
-/*チェックボックス等は非表示に*/
+/* チェックボックス等は非表示に */
 .nav-unshown {
-  display:none;
+  display: none;
 }
 
-/*アイコンのスペース*/
+/* アイコンのスペース */
 #nav-open {
   display: inline-block;
   width: 30px;
@@ -56,11 +67,13 @@ export default {
   vertical-align: middle;
 }
 
-/*ハンバーガーアイコンをCSSだけで表現*/
-#nav-open span, #nav-open span:before, #nav-open span:after {
+/* ハンバーガーアイコンをCSSだけで表現 */
+#nav-open span,
+#nav-open span::before,
+#nav-open span::after {
   position: absolute;
-  height: 3px;/*線の太さ*/
-  width: 25px;/*長さ*/
+  height: 3px;/* 線の太さ */
+  width: 25px;/* 長さ */
   border-radius: 3px;
   background: #555;
   display: block;
@@ -68,53 +81,53 @@ export default {
   cursor: pointer;
 }
 
-#nav-open span:before {
+#nav-open span::before {
   bottom: -8px;
 }
 
-#nav-open span:after {
+#nav-open span::after {
   bottom: -16px;
 }
 
-/*閉じる用の薄黒カバー*/
+/* 閉じる用の薄黒カバー */
 #nav-close {
-  display: none;/*はじめは隠しておく*/
+  display: none;/* はじめは隠しておく */
   position: fixed;
   z-index: 99;
-  top: 0;/*全体に広がるように*/
+  top: 0;/* 全体に広がるように */
   left: 0;
   width: 100%;
   height: 100%;
   background: black;
   opacity: 0;
-  transition: .3s ease-in-out;
+  transition: 0.3s ease-in-out;
 }
 
-/*中身*/
+/* 中身 */
 #nav-content {
   overflow: auto;
   position: fixed;
   top: 0;
   left: 0;
-  z-index: 9999;/*最前面に*/
-  width: 90%;/*右側に隙間を作る（閉じるカバーを表示）*/
-  max-width: 330px;/*最大幅（調整してください）*/
+  z-index: 9999;/* 最前面に */
+  width: 90%;/* 右側に隙間を作る（閉じるカバーを表示） */
+  max-width: 330px;/* 最大幅（調整してください） */
   height: 100%;
-  background: #fff;/*背景色*/
-  transition: .3s ease-in-out;/*滑らかに表示*/
+  background: #fff;/* 背景色 */
+  transition: 0.3s ease-in-out;/* 滑らかに表示 */
   -webkit-transform: translateX(-105%);
-  transform: translateX(-105%);/*左に隠しておく*/
+  transform: translateX(-105%);/* 左に隠しておく */
 }
 
-/*チェックが入ったらもろもろ表示*/
+/* チェックが入ったらもろもろ表示 */
 #nav-input:checked ~ #nav-close {
-  display: block;/*カバーを表示*/
-  opacity: .5;
+  display: block;/* カバーを表示 */
+  opacity: 0.5;
 }
 
 #nav-input:checked ~ #nav-content {
   -webkit-transform: translateX(0%);
-  transform: translateX(0%);/*中身を表示（右へスライド）*/
-  box-shadow: 6px 0 25px rgba(0,0,0,.15);
+  transform: translateX(0%);/* 中身を表示（右へスライド） */
+  box-shadow: 6px 0 25px rgba(0, 0, 0, 0.15);
 }
 </style>
