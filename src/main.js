a1f787ac75561cb10187fb40a403f0a5977c7643
岡崎悠 <okazakiyu@okazakiyuunoMacBook-Air.local>
2020-04-21 12:23:10 +0900

    commit

--- src/main.js
+++ src/main.js
@@ -2,15 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import './assets/css/reset.css'
 import smoothScroll from 'vue-smoothscroll'
-import axios from 'axios'
 import VueAxios from 'vue-axios'
+import store from './store'
 
-Vue.use(VueAxios, axios)
-Vue.config.productionTip = false
-
+Vue.use(VueAxios)
 Vue.use(smoothScroll)
 Vue.config.productionTip = false
 
 new Vue({
+  store,
   render: h => h(App),
 }).$mount('#app')
--- src/store.js
+++ src/store.js
@@ -1,38 +1,37 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios'
 
 Vue.use(Vuex)
 
-const store = new Vuex.Store({
-
-  //state:コンポーネントでいうdata
+export default new Vuex.Store({
   state: {
-    chart: 'front'
+    skillCategories: []
   },
 
-  //getters:コンポーネントでいうcomputed的なもの
-  getters:{
-    //messageを使用するgetter
-    chart(state) {
-      return state.chart
-    }
+  getters: {
+    getSkills: (state) => (category) => {
+      if(state.skillCategories.length > 0) {
+        return state.skillCategories.find((skill) => skill.category === category);
+      }
+      return [];
+    },
   },
 
-  //mutations:コンポーネントでいうmethod（と言うかsetter）
-  //stateを唯一変更できるもの
   mutations: {
-    //vuexでは引数をpayloadと呼ぶっぽい
-    //payloadはオブジェクトにするべき（いっぱい入れれるし）
-    setMessage(state,payload){
-      state.chart = payload.chart
-    }
+    setSkillCategories(state, payload) {
+      state.skillCategories = payload.skillCategories;
+    },
   },
 
-  //actionのコミットを使うことでミューテーションを呼び出す（コンポーネントには無い概念）
   actions: {
-    doUpdate({commit}, chart){
-      commit('setMessage',{chart})
-    }
-  }
+    async updateSkillCategories({commit}) {
+      const skillCategories = [];
+      const res = await axios.get('https://us-central1-portfolio-c4799.cloudfunctions.net/skills')
+      res.data.forEach((category) => {
+        skillCategories.push(category);
+      });
+      commit('setSkillCategories', {skillCategories});
+    },
+  },
 })
-export default store
